f51fb2120762bec8f4ed52bd2a1e0b78374607dd
Gabriel Wolf <gwolf@student.42vienna.com>
2023-09-14 09:40:01 +0200

    feat: phonebook input reading start

--- Module00/ex01/PhoneBook.cpp
+++ Module00/ex01/PhoneBook.cpp
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/07 20:23:43 by gwolf             #+#    #+#             */
-/*   Updated: 2023/09/07 22:34:18 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/09/14 09:38:43 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,4 +18,32 @@ PhoneBook::PhoneBook(void)
 
 PhoneBook::~PhoneBook(void)
 {
+}
+
+void	PhoneBook::AddContact(void)
+{
+	std::string input[5];
+
+	do
+	{
+		std::cout << "Please enter first name: ";
+		std::getline(std::cin, input[0]);
+		if (std::cin.eof())
+			continue;
+		if (input[0].empty())
+			std::cout << "Should not be empty!" << std::endl;
+	} while (input[0].empty());
+
+
+	std::cout << "Please enter last name: ";
+	std::cin >> input[1];
+	std::cout << "Please enter nick name: ";
+	std::cin >> input[2];
+	std::cout << "Please enter phone number: ";
+	std::cin >> input[3];
+	std::cout << "Please enter darkest secret: ";
+	std::cin >> input[4];
+	contact[0].SetInfo(input);
+
+
 }
--- Module00/ex01/PhoneBook.hpp
+++ Module00/ex01/PhoneBook.hpp
@@ -6,20 +6,26 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/07 20:16:22 by gwolf             #+#    #+#             */
-/*   Updated: 2023/09/07 22:23:36 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/09/11 08:59:00 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHONEBOOK_HPP
 # define PHONEBOOK_HPP
 
-#include <iostream>
+# include <iostream>
+# include "Contact.hpp"
 
 class PhoneBook
 {
 public:
 	PhoneBook( void );
 	~PhoneBook( void );
+	void AddContact(void);
+
+private:
+	Contact contact[8];
+
 };
 
 #endif
