4f32fe0bbbd6dee1ae1dce96af2cc471593e7af7
Gabriel <gwolf@student.42vienna.com>
2023-09-14 19:23:22 +0200

    feat: add loop

--- Module00/ex01/main.cpp
+++ Module00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/07 22:06:57 by gwolf             #+#    #+#             */
-/*   Updated: 2023/09/14 08:18:36 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/09/14 18:59:11 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,34 +29,53 @@ e_choice resolveChoice(std::string input)
 	else return INVALID;
 }
 
-int	main(void)
+void	printOptions(void)
 {
-	PhoneBook phonebook;
-	std::string input;
-
 	std::cout << "ADD: save a new contact" << std::endl;
 	std::cout << "SEARCH: display a specific contact" << std::endl;
 	std::cout << "EXIT: exit the program" << std::endl;
-	std::cout << "Please enter a keyword: ";
-	std::getline(std::cin, input);
+	std::cout << std::endl << "Please enter a keyword: ";
+}
 
-	switch (resolveChoice(input))
+int	main(void)
+{
+	PhoneBook 	phonebook;
+	std::string input;
+	bool		loop;
+
+	loop = true;
+	std::cout << "Very cool phonebook program!" << std::endl << std::endl;
+	do
 	{
-		case ADD:
-			phonebook.AddContact();
+		printOptions();
+	
+		if (!getline(std::cin, input))
+		{
+			std::cout << "EOF received - Aborting." << std::endl;
 			break;
+		}
 
-		case SEARCH:
-			std::cout << "SEARCH was entered" << std::endl;
-			break;
+		switch (resolveChoice(input))
+		{
+			case ADD:
+				phonebook.AddContact();
+				break;
 
-		case EXIT:
-			std::cout << "EXIT was entered" << std::endl;
-			break;
+			case SEARCH:
+				std::cout << "SEARCH was entered" << std::endl;
+				break;
 
-		default:
-			std::cout << "Not recognised" << std::endl;
-			break;
-	}
+			case EXIT:
+				std::cout << "EXIT was entered - goodbye" << std::endl;
+				loop = false;
+				break;
+
+			default:
+				std::cout << "\"" << input << "\": Not recognised" << std::endl << std::endl;
+				break;
+		}
+	} while (loop);
+
+
 
 }
