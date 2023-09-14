8b86053fb8d54501f03d744b170fa5b9a6ee4582
Gabriel Wolf <gwolf@student.42vienna.com>
2023-09-14 09:39:39 +0200

    feat: add Contact class

--- Module00/ex01/Contact.cpp
+++ Module00/ex01/Contact.cpp
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/07 20:25:54 by gwolf             #+#    #+#             */
-/*   Updated: 2023/09/07 22:35:13 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/09/11 09:21:08 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,4 +18,13 @@ Contact::Contact(void)
 
 Contact::~Contact(void)
 {
+}
+
+void	Contact::SetInfo(std::string info[5])
+{
+	first_name = info[0];
+	last_name = info[1];
+	nick_name = info[2];
+	phone_number = info[3];
+	darkest_secret = info[4];
 }
--- Module00/ex01/Contact.hpp
+++ Module00/ex01/Contact.hpp
@@ -6,22 +6,29 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/07 20:25:58 by gwolf             #+#    #+#             */
-/*   Updated: 2023/09/07 22:35:01 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/09/11 14:55:04 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CONTACT_HPP
 # define CONTACT_HPP
 
-#include <iostream>
+# include <iostream>
+# include <string>
 
 class Contact
 {
 private:
-	/* data */
+	std::string first_name;
+	std::string last_name;
+	std::string nick_name;
+	std::string phone_number;
+	std::string darkest_secret;
+
 public:
 	Contact(void);
 	~Contact(void);
+	void	SetInfo(std::string info[5]);
 };
 
 
