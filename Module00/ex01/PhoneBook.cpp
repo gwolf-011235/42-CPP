5322f1cf2742a02e875f48118523bd7346bdee4e
Gabriel Wolf <gwolf@student.42vienna.com>
2023-09-15 12:28:38 +0200

    feat: start of search function

--- Module00/ex01/PhoneBook.cpp
+++ Module00/ex01/PhoneBook.cpp
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/07 20:23:43 by gwolf             #+#    #+#             */
-/*   Updated: 2023/09/14 19:19:55 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/09/15 12:17:36 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 PhoneBook::PhoneBook(void)
 {
+	m_filled_contacts = 0;
 }
 
 PhoneBook::~PhoneBook(void)
@@ -42,7 +43,8 @@ int		PhoneBook::ReceiveInput(const std::string& ref, std::string& input)
 
 bool	PhoneBook::AddContact(void)
 {
-	std::string input[5];
+	std::string 			input[5];
+	static unsigned char	index;
 
 	if (ReceiveInput("first name", input[0]))
 		return (true);
@@ -54,6 +56,36 @@ bool	PhoneBook::AddContact(void)
 		return (true);
 	if (ReceiveInput("darkest secret", input[4]))
 		return (true);
-	m_contact[0].SetInfo(input);
+
+	m_contact[index].SetInfo(input[0], input[1], input[2], input[3], input[4]);
+	if (m_filled_contacts < NUM_CONTACTS)
+		m_filled_contacts++;
+	index++;
+	if (index >= NUM_CONTACTS)
+		index = 0;
+	return (false);
+}
+
+std::string&	PhoneBook::Truncate(std::string& str, size_t width)
+{
+	if (str.length() > width)
+	{
+		str.resize(width - 1);
+		return (str.append("."));
+	}
+	return (str);
+}
+bool	PhoneBook::SearchContact(void)
+{
+	std::string temp[3];
+	for (int i = 0; i < m_filled_contacts; i++)
+	{
+		m_contact[i].GetFirstName(temp[0]);
+		m_contact[i].GetLastName(temp[1]);
+		m_contact[i].GetNickName(temp[2]);
+		std::cout << std::setw(10) << Truncate(temp[0], 10) << "|";
+		std::cout << std::setw(10) << Truncate(temp[1], 10) << "|";
+		std::cout << std::setw(10) << Truncate(temp[2], 10) << "|" << std::endl;
+	}
 	return (false);
 }
--- Module00/ex01/PhoneBook.hpp
+++ Module00/ex01/PhoneBook.hpp
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/07 20:16:22 by gwolf             #+#    #+#             */
-/*   Updated: 2023/09/14 13:48:14 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/09/15 12:02:53 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,26 @@
 # define PHONEBOOK_HPP
 
 # include <iostream>
+# include <iomanip>
 # include "Contact.hpp"
+
+# define NUM_CONTACTS 8
 
 class PhoneBook
 {
 public:
 	PhoneBook( void );
 	~PhoneBook( void );
-	bool AddContact(void);
+
+	bool	AddContact(void);
+	bool	SearchContact(void);
 
 private:
-	Contact m_contact[8];
-	int	ReceiveInput(const std::string& ref, std::string& input);
+	Contact m_contact[NUM_CONTACTS];
+	int		m_filled_contacts;
+
+	int				ReceiveInput(const std::string& ref, std::string& input);
+	std::string&	Truncate(std::string& str, size_t width);
 
 };
 
