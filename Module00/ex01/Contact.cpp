e2e413744178e67555ca8a8f470922e31edf8dd2
Gabriel Wolf <gwolf@student.42vienna.com>
2023-09-15 12:28:18 +0200

    feat: added contact getters

--- Module00/ex01/Contact.cpp
+++ Module00/ex01/Contact.cpp
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/07 20:25:54 by gwolf             #+#    #+#             */
-/*   Updated: 2023/09/11 09:21:08 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/09/15 11:46:54by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,37 @@ Contact::~Contact(void)
 {
 }
 
-void	Contact::SetInfo(std::string info[5])
+void	Contact::SetInfo(std::string& first_name, std::string& last_name, std::string& nick_name,
+						std::string& phonenumber, std::string& darkest_secret)
 {
-	first_name = info[0];
-	last_name = info[1];
-	nick_name = info[2];
-	phone_number = info[3];
-	darkest_secret = info[4];
+	m_first_name = first_name;
+	m_last_name = last_name;
+	m_nick_name = nick_name;
+	m_phone_number = phonenumber;
+	m_darkest_secret = darkest_secret;
+}
+
+void	Contact::GetFirstName(std::string& first_name)
+{
+	first_name = m_first_name;
+}
+
+void	Contact::GetLastName(std::string& last_name)
+{
+	last_name = m_last_name;
+}
+
+void	Contact::GetNickName(std::string& nick_name)
+{
+	nick_name = m_nick_name;
+}
+
+void	Contact::GetPhonenumber(std::string& phonenumber)
+{
+	phonenumber = m_phone_number;
+}
+
+void	Contact::GetDarkestSecret(std::string& darkest_secret)
+{
+	darkest_secret = m_darkest_secret;
 }
--- Module00/ex01/Contact.hpp
+++ Module00/ex01/Contact.hpp
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/07 20:25:58 by gwolf             #+#    #+#             */
-/*   Updated: 2023/09/11 14:55:04 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/09/15 12:08:06 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,23 @@
 class Contact
 {
 private:
-	std::string first_name;
-	std::string last_name;
-	std::string nick_name;
-	std::string phone_number;
-	std::string darkest_secret;
+	std::string m_first_name;
+	std::string m_last_name;
+	std::string m_nick_name;
+	std::string m_phone_number;
+	std::string m_darkest_secret;
 
 public:
 	Contact(void);
 	~Contact(void);
-	void	SetInfo(std::string info[5]);
+	void	SetInfo(std::string& first_name, std::string& last_name,
+					std::string& nick_name, std::string& phonenumber,
+					std::string& darkest_secret);
+	void	GetFirstName(std::string& first_name);
+	void	GetLastName(std::string& last_name);
+	void	GetNickName(std::string& nick_name);
+	void	GetPhonenumber(std::string& phonenumber);
+	void	GetDarkestSecret(std::string& darkest_secret);
 };
-
 
 #endif
