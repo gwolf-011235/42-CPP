5643af601de9ee74f59d806bc2e9bcfadaf16465
Gabriel Wolf <gwolf@student.42vienna.com>
2023-09-17 10:50:36 +0200

    feat: add several method definitions

--- Module00/ex02/Account.cpp
+++ Module00/ex02/Account.cpp
@@ -6,9 +6,119 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/17 09:27:11 by gwolf             #+#    #+#             */
-/*   Updated: 2023/09/17 09:27:20 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/09/17 10:49:42 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
+#include <iostream>
+#include <sstream>
+#include <ctime>
 
+/* Static variables */
+
+int	Account::_nbAccounts;
+int	Account::_totalAmount;
+int	Account::_totalNbDeposits;
+int	Account::_totalNbWithdrawals;
+
+/* Constructor - Destructor */
+
+Account::Account( void )
+{
+}
+
+Account::Account( int initial_deposit ) : _amount(initial_deposit)
+{
+	Account::_nbAccounts++;
+	Account::_totalAmount += initial_deposit;
+	_accountIndex = Account::_nbAccounts;
+	_nbDeposits = 0;
+	_nbWithdrawals = 0;
+}
+
+Account::~Account( void )
+{
+	Account::_nbAccounts--;
+	Account::_totalAmount -= _amount;
+}
+
+/* Methods */
+
+int	Account::getNbAccounts( void )
+{
+	return Account::_nbAccounts;
+}
+
+int	Account::getTotalAmount( void )
+{
+	return Account::_totalAmount;
+}
+
+int	Account::getNbDeposits( void )
+{
+	return Account::_totalNbDeposits;
+}
+
+int	Account::getNbWithdrawals( void )
+{
+	return Account::_totalNbWithdrawals;
+}
+
+void	Account::displayAccountsInfos( void )
+{
+
+}
+
+void	Account::makeDeposit( int deposit )
+{
+	_amount += deposit;
+	_nbDeposits++;
+	Account::_totalNbDeposits++;
+	Account::_totalAmount += deposit;
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";";
+	std::cout << "p_amount:" << _amount - deposit << ";";
+	std::cout << "deposit:" << deposit << ";";
+	std::cout << "amount:" << _amount << ";";
+	std::cout << "nb_deposit:" << _nbDeposits << std::endl;
+}
+
+bool	Account::makeWithdrawal( int withdrawal )
+{
+	if (_amount >= withdrawal) {
+		_amount -= withdrawal;
+		return true;
+	} else {
+		std::cout << "refused" << std::endl;
+		return false;
+	}
+}
+
+int		Account::checkAmount( void ) const
+{
+	return _amount;
+}
+
+void	Account::displayStatus( void ) const
+{
+}
+
+void	Account::_displayTimestamp( void )
+{
+	time_t 		now = time(0);
+	struct tm	tstruct;
+	char		buf[80];
+
+	tstruct = *localtime(&now);
+	strftime(buf, sizeof(buf), "%Y%m%d_%H%M%S", &tstruct);
+
+	// Format the timestamp
+	std::ostringstream timestamp;
+	timestamp << "[";
+	timestamp << buf;
+	timestamp << "] ";
+
+	// Display the timestamp
+	std::cout << timestamp.str();
+}
