23351fb909b5e5f38c7b34f674b8f6f6054f0c02
Gabriel Wolf <gwolf@student.42vienna.com>
2023-09-18 21:54:08 +0200

    fix: sugarize

--- Module01/ex05/Harl.cpp
+++ Module01/ex05/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/17 19:27:26 by gwolf             #+#    #+#             */
-/*   Updated: 2023/09/18 21:44:45 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/09/18 21:48:32 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,22 +22,25 @@ Harl::~Harl( void )
 
 void	Harl::debug( void )
 {
+	std::cout << "[ DEBUG ]\n";
 	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
 }
 
 void	Harl::info( void )
 {
+	std::cout << "[ INFO ]\n";
 	std::cout << "I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!" << std::endl;
 }
 
 void	Harl::warning( void )
 {
+	std::cout << "[ WARNING ]\n";
 	std::cout << "I think I deserve to have some extra bacon for free. I've been coming for years whereas you started working here since last month." << std::endl;
-
 }
 
 void	Harl::error( void )
 {
+	std::cout << "[ ERROR ]\n";
 	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
 }
 
--- Module01/ex05/main.cpp
+++ Module01/ex05/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/17 19:24:42 by gwolf             #+#    #+#             */
-/*   Updated: 2023/09/18 21:34:03 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/09/18 21:53:32 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,11 @@
 int main(void)
 {
 	Harl harl;
+	std::string level[5] = {"debug", "warning", "info", "error", "noexist"};
 
-	std::cout << "Complain level debug: \n";
-	harl.complain("debug");
-	std::cout << "Complain level warning: \n";
-	harl.complain("warning");
-	std::cout << "Complain level info: \n";
-	harl.complain("info");
-	std::cout << "Complain level error: \n";
-	harl.complain("error");
-	std::cout << "Complain level noexist: \n";
-	harl.complain("noexist");
+	for (int i = 0; i < 5; ++i) {
+		std::cout << "Complain level " << level[i] << ": \n";
+		harl.complain(level[i]);
+		std::cout << '\n';
+	}
 }
