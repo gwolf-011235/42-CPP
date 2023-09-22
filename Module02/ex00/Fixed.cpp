e2804413213a3fd8b31529c84ad31e241285ba1c
Gabriel Wolf <gwolf@student.42vienna.com>
2023-09-22 14:48:22 +0200

    fix: solution for 02/00

--- Module02/ex00/Fixed.cpp
+++ Module02/ex00/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/22 14:07:09 by gwolf             #+#    #+#             */
-/*   Updated: 2023/09/22 14:32:10 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/09/22 14:47:37 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,30 +14,31 @@
 
 Fixed::Fixed(void) : m_value(0)
 {
-	std::cout << "Fixed default constructor called\n";
+	std::cout << "Default constructor called: Fixed\n";
 }
 
 Fixed::Fixed(const Fixed& ref)
 {
-	std::cout << "Fixed copy constructor called\n";
-	m_value = ref.m_value;
+	std::cout << "Copy constructor called: Fixed\n";
+	*this = ref;
 }
 
 Fixed& Fixed::operator=(const Fixed& ref)
 {
-	if (this == &ref)
-		return *this;
-	return *this(ref);
+	if (&ref != this) {
+		this->m_value = ref.getRawBits();
+	}
+	return *this;
 }
 
 Fixed::~Fixed(void)
 {
-	std::cout << "Fixed destructor called\n";
+	std::cout << "Destructor called: Fixed\n";
 }
 
 int		Fixed::getRawBits( void ) const
 {
-	std::cout << "Fixed getRawBits member function called\n";
+	std::cout << "getRawBits member function called: Fixed\n";
 	return m_value;
 }
 
