4cd67b638685defb3f0946f38dc7f3686c6b719c
Gabriel <gwolf@student.42vienna.com>
2023-09-26 14:11:08 +0200

    fix: cleanup

--- Module01/ex04/main.cpp
+++ Module01/ex04/main.cpp
@@ -6,31 +6,25 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/17 16:48:53 by gwolf             #+#    #+#             */
-/*   Updated: 2023/09/21 14:22:38 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/09/26 14:10:57 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
 
-// Function to replace all occurrences of 'search' with 'replace' in 'str'
 std::string custom_replace(const std::string& str, const std::string& search, const std::string& replace)
 {
 	std::string result;
 
-	size_t startPos = 0; // Start position for searching 'search'
-	size_t foundPos;     // Position where 'search' was found
+	size_t startPos = 0;
+	size_t foundPos;
 
 	while ((foundPos = str.find(search, startPos)) != std::string::npos) {
-		// Append characters from 'startPos' to 'foundPos'
 		result.append(str, startPos, foundPos - startPos);
-		// Append the 'replace' string
 		result.append(replace);
-		// Update 'startPos' to the position after the 'search' string
 		startPos = foundPos + search.length();
 	}
-
-	// Append characters after the last occurrence of 'search'
 	result.append(str, startPos);
 
 	return result;
