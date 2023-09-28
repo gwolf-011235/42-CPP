ef57b1496dc2abc119ce9974f04dd472359b006c
Gabriel <gwolf@student.42vienna.com>
2023-09-28 18:43:58 +0200

    feat: first 02/03

--- Module02/ex03/Point.cpp
+++ Module02/ex03/Point.cpp
@@ -6,40 +6,31 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 15:19:47 by gwolf             #+#    #+#             */
-/*   Updated: 2023/09/26 18:40:17 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/09/28 17:56:19 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
 
-Point::Point(void) : m_x(0), m_y(0), m_z(0)
+Point::Point(void) : m_x(0), m_y(0)
 {
 	std::cout << "Default constructor called: Point \n";
 }
 
-Point::Point(const Point& ref)
-{
-	std::cout << "Copy constructor called: Point \n";
-	*this = ref;
-}
-
-Point::Point( float x, float y, float z): m_x(x), m_y(y), m_z(z)
+Point::Point(const float x, const float y): m_x(x), m_y(y)
 {
 	std::cout << "Float constructor called: Point \n";
 }
 
-Point::Point( int x, int y, int z)
+Point::Point(const Point& ref) : m_x(ref.m_x), m_y(ref.m_y)
 {
-	std::cout << "Int constructor called: Point \n";
-
+	std::cout << "Copy constructor called: Point \n";
 }
 
 Point& Point::operator=(const Point& ref)
 {
 	if (&ref != this) {
-		m_x = ref.m_x;
-		m_y = ref.m_y;
-		m_z = ref.m_z;
+		std::cout << "Copy assignment called: Point - everything const oh no!\n";
 	}
 	return *this;
 }
@@ -47,4 +38,14 @@ Point& Point::operator=(const Point& ref)
 Point::~Point(void)
 {
 	std::cout << "Destructor called: Point \n";
+}
+
+float	Point::get_float_x() const
+{
+	return m_x.toFloat();
+}
+
+float	Point::get_float_y() const
+{
+	return m_y.toFloat();
 }
--- Module02/ex03/bsp.cpp
+++ Module02/ex03/bsp.cpp
@@ -1,32 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Point.hpp                                          :+:      :+:    :+:   */
+/*   bsp.cpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/26 15:19:19 by gwolf             #+#    #+#             */
-/*   Updated: 2023/09/28 17:55:48 by gwolf            ###   ########.fr       */
+/*   Created: 2023/09/28 17:29:35 by gwolf             #+#    #+#             */
+/*   Updated: 2023/09/28 17:57:44 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#pragma once
+#include "Point.hpp"
 
-#include <iostream>
-#include "Fixed.hpp"
-
-class Point
+bool bsp( Point const a, Point const b, Point const c, Point const point)
 {
-private:
-	const Fixed m_x;
-	const Fixed m_y;
-public:
-	Point(void);
-	Point(const float x, const float y);
-	Point(const Point& ref);
-	Point& operator= (const Point& ref);
-	~Point(void);
-	float	get_float_x() const;
-	float	get_float_y() const;
-};
-
+	double denominator = (b.get_float_y() - c.get_float_y()) * (a.get_float_x() - c.get_float_x()) + (c.get_float_x())
+}
