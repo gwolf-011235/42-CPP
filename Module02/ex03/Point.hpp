67aa315e867d46bc1e30ca11d4d91fa4de53f91a
Gabriel Wolf <gwolf@student.42vienna.com>
2023-10-02 09:36:40 +0200

    fix: rename to shorter

--- Module02/ex03/Point.hpp
+++ Module02/ex03/Point.hpp
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 15:19:19 by gwolf             #+#    #+#             */
-/*   Updated: 2023/09/28 17:55:48 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/10/02 08:47:38 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ public:
 	Point(const Point& ref);
 	Point& operator= (const Point& ref);
 	~Point(void);
-	float	get_float_x() const;
-	float	get_float_y() const;
+	float	get_xf() const;
+	float	get_yf() const;
 };
 
