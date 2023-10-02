3d7c19e0bc83e31e495939504a9bcd6fba6226fb
Gabriel <gwolf@student.42vienna.com>
2023-10-02 11:58:06 +0200

    feat: implement triagnel drawing

--- Module02/ex03/main.cpp
+++ Module02/ex03/main.cpp
@@ -6,32 +6,46 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/22 14:07:28 by gwolf             #+#    #+#             */
-/*   Updated: 2023/10/02 09:35:15 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/10/02 11:30:13 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 #include "Point.hpp"
 bool bsp( Point const a, Point const b, Point const c, Point const point);
-int promptInput(void);
+void	promptInput(float x[4], float y[4]);
+void	init(int argc, char **argv);
+
+Point* a;
+Point* b;
+Point* c;
+Point* p;
+bool	inside;
 
 #include <iostream>
-#include <limits> // For numeric_limits
 
-int main( void )
+int main(int argc, char **argv)
 {
-	Point a(0.0, 0.0);
-	Point b(1.0, 0.0);
-	Point c(0.5, 1.0);
+	float x[4], y[4];
+	promptInput(x, y);
 
-	Point x(0.5, 0.5);
+	a = new Point(x[0], y[0]);
+	b = new Point(x[1], y[1]);
+	c = new Point(x[2], y[2]);
+	p = new Point(x[3], y[3]);
 
-	promptInput();
-	if (bsp(a, b, c, x)) {
+	inside = bsp(*a, *b, *c, *p);
+	if (inside) {
 			std::cout << "The point is inside the triangle.\n";
 	} else {
 			std::cout << "The point is outside the triangle.\n";
 	}
+
+	init(argc, argv);
+	delete a;
+	delete b;
+	delete c;
+	delete p;
 	return 0;
 }
 
