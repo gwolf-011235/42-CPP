46d452519e46a23b57e9971db6919d5e8df2e862
Gabriel <gwolf@student.42vienna.com>
2023-10-02 11:57:44 +0200

    feat: finish input handling

--- Module02/ex03/input.cpp
+++ Module02/ex03/input.cpp
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 09:21:22 by gwolf             #+#    #+#             */
-/*   Updated: 2023/10/02 09:35:45 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/10/02 11:57:23 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,6 @@ void	getInputAndStore(float& value)
 	std::string input;
 	std::cin >> input;
 
-	// Input validation for a valid float
 	while (!isValidFloat(input, value)) {
 		std::cout << "Invalid input. Please enter a valid float: ";
 		clearAndIgnoreInvalidInput();
@@ -52,26 +51,19 @@ void	getInputAndStore(float& value)
 	}
 }
 
-int promptInput(void)
+void	promptInput(float x[4], float y[4])
 {
-	float x[4], y[4];
 	std::string input;
 
 	for (int i = 0; i < 4; ++i) {
-		// Prompt the user to input the x-coordinate
 		std::cout << "Enter the x-coordinate for point " << i + 1 << ": ";
 		getInputAndStore(x[i]);
 
-		// Prompt the user to input the y-coordinate
 		std::cout << "Enter the y-coordinate for point " << i + 1 << ": ";
 		getInputAndStore(y[i]);
 	}
 
-	// Now, you have the x and y coordinates stored in the arrays x[] and y[].
-
 	for (int i = 0; i < 4; ++i) {
 		std::cout << "Point " << i + 1 << ": (" << x[i] << ", " << y[i] << ")\n";
 	}
-
-	return 0;
 }
