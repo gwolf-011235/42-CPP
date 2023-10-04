e2bf18d17ca2d4965278007937a71dd5c535ccc3
Gabriel <gwolf@student.42vienna.com>
2023-10-04 12:03:02 +0200

    fix: only accept points inside the triangle

--- Module02/ex03/bsp.cpp
+++ Module02/ex03/bsp.cpp
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/28 17:29:35 by gwolf             #+#    #+#             */
-/*   Updated: 2023/10/04 09:56:18 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/10/04 11:37:18 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ bool bsp( Point const a, Point const b, Point const c, Point const p)
 	double lambda2 = ((c.get_yf() - a.get_yf()) * (p.get_xf() - c.get_xf()) + (a.get_xf() - c.get_xf()) * (p.get_yf() - c.get_yf())) / denominator;
 	double lambda3 = 1.0 - lambda1 - lambda2;
 
-	return (lambda1 >= 0.0 && lambda1 <= 1.0 &&
-			lambda2 >= 0.0 && lambda2 <= 1.0 &&
-			lambda3 >= 0.0 && lambda3 <= 1.0);
+	return (lambda1 > 0.0 && lambda1 < 1.0 &&
+			lambda2 > 0.0 && lambda2 < 1.0 &&
+			lambda3 > 0.0 && lambda3 < 1.0);
 }
