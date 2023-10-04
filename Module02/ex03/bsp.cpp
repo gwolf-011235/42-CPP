64a7433c47d2bd59aa6d532b9bc6ae25e77cfb90
Gabriel <gwolf@student.42vienna.com>
2023-10-04 10:09:12 +0200

    fix: change point to p

--- Module02/ex03/bsp.cpp
+++ Module02/ex03/bsp.cpp
@@ -6,17 +6,17 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/28 17:29:35 by gwolf             #+#    #+#             */
-/*   Updated: 2023/10/02 08:51:34 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/10/04 09:56:18 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
 
-bool bsp( Point const a, Point const b, Point const c, Point const point)
+bool bsp( Point const a, Point const b, Point const c, Point const p)
 {
 	double denominator = (b.get_yf() - c.get_yf()) * (a.get_xf() - c.get_xf()) + (c.get_xf()) * (a.get_yf() - c.get_yf());
-	double lambda1 = ((b.get_yf() - c.get_yf()) * (point.get_xf() - c.get_xf()) + (c.get_xf() - b.get_xf()) * (point.get_yf() - c.get_yf())) / denominator;
-	double lambda2 = ((c.get_yf() - a.get_yf()) * (point.get_xf() - c.get_xf()) + (a.get_xf() - c.get_xf()) * (point.get_yf() - c.get_yf())) / denominator;
+	double lambda1 = ((b.get_yf() - c.get_yf()) * (p.get_xf() - c.get_xf()) + (c.get_xf() - b.get_xf()) * (p.get_yf() - c.get_yf())) / denominator;
+	double lambda2 = ((c.get_yf() - a.get_yf()) * (p.get_xf() - c.get_xf()) + (a.get_xf() - c.get_xf()) * (p.get_yf() - c.get_yf())) / denominator;
 	double lambda3 = 1.0 - lambda1 - lambda2;
 
 	return (lambda1 >= 0.0 && lambda1 <= 1.0 &&
