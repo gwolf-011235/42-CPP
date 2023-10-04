3091a43ce7bfde369227cdfa026f373760f558fc
Gabriel <gwolf@student.42vienna.com>
2023-10-04 10:46:19 +0200

    feat: new mini main

--- Module02/ex03/main.cpp
+++ Module02/ex03/main.cpp
@@ -6,46 +6,32 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/22 14:07:28 by gwolf             #+#    #+#             */
-/*   Updated: 2023/10/02 11:30:13 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/10/04 10:30:03 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 #include "Point.hpp"
-bool bsp( Point const a, Point const b, Point const c, Point const point);
-void	promptInput(float x[4], float y[4]);
-void	init(int argc, char **argv);
+bool 	bsp( Point const a, Point const b, Point const c, Point const p);
 
-Point* a;
-Point* b;
-Point* c;
-Point* p;
-bool	inside;
-
-#include <iostream>
-
-int main(int argc, char **argv)
+int main(void)
 {
-	float x[4], y[4];
-	promptInput(x, y);
+	Point a(0, 0);
+	Point b(1, 1);
+	Point c(1, 0);
+	Point p(0.9, 0.5);
 
-	a = new Point(x[0], y[0]);
-	b = new Point(x[1], y[1]);
-	c = new Point(x[2], y[2]);
-	p = new Point(x[3], y[3]);
+	std::cout << "Point a: (" << a.get_xf() << " " << a.get_yf() << ")\n";
+	std::cout << "Point b: (" << b.get_xf() << " " << b.get_yf() << ")\n";
+	std::cout << "Point c: (" << c.get_xf() << " " << c.get_yf() << ")\n";
+	std::cout << "Point p: (" << p.get_xf() << " " << p.get_yf() << ")\n";
 
-	inside = bsp(*a, *b, *c, *p);
-	if (inside) {
+	if (bsp(a, b, c, p)) {
 			std::cout << "The point is inside the triangle.\n";
 	} else {
 			std::cout << "The point is outside the triangle.\n";
 	}
 
-	init(argc, argv);
-	delete a;
-	delete b;
-	delete c;
-	delete p;
 	return 0;
 }
 
