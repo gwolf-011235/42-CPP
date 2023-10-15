6f73c3941b409fc210016b9870a0e350a280b08a
Gabriel Wolf <gwolf@student.42vienna.com>
2023-10-15 21:54:59 +0200

    feat: implement AMateria

--- Module04/ex03/AMateria.cpp
+++ Module04/ex03/AMateria.cpp
@@ -6,9 +6,28 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/15 21:08:10 by gwolf             #+#    #+#             */
-/*   Updated: 2023/10/15 21:08:19 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/10/15 21:52:02 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "AMateria.hpp"
 
+AMateria::AMateria(std::string const & type) : m_type(type)
+{
+	std::cout << "Default constructor called: AMateria \n";
+}
+
+AMateria::~AMateria()
+{
+	std::cout << "Destructor called: AMateria \n";
+}
+
+std::string const & AMateria::getType() const
+{
+	return m_type;
+}
+
+void AMateria::use(ICharacter& target)
+{
+	std::cout << "*The materia " << getType() << " was used on " << target.getName() << "*\n";
+}
--- Module04/ex03/AMateria.hpp
+++ Module04/ex03/AMateria.hpp
@@ -6,22 +6,23 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/15 21:06:53 by gwolf             #+#    #+#             */
-/*   Updated: 2023/10/15 21:17:39 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/10/15 21:42:12 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
+#include <iostream>
 #include <string>
 #include "ICharacter.hpp"
 
 class AMateria
 {
 protected:
-	//[...]
+	const std::string m_type;
 public:
 	AMateria(std::string const & type);
-	//[...]
+	virtual ~AMateria();
 	std::string const & getType() const; //Returns the materia type
 	virtual AMateria* clone() const = 0;
 	virtual void use(ICharacter& target);
