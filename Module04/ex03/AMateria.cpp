c34cb674a83ff51942051933b866cb1d85185d07
Gabriel Wolf <gwolf@student.42vienna.com>
2023-10-16 23:15:39 +0200

    feat: implement Floor in AMateria

--- Module04/ex03/AMateria.cpp
+++ Module04/ex03/AMateria.cpp
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/15 21:08:10 by gwolf             #+#    #+#             */
-/*   Updated: 2023/10/16 20:40:44 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/10/16 23:13:23 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,23 @@ std::string const & AMateria::getType() const
 
 void AMateria::use(ICharacter& target)
 {
-	//std::cout << "*The materia " << getType() << " was used on " << target.getName() << "*\n";
-	(void)target;
+	std::cout << "*The materia " << getType() << " was used on " << target.getName() << "*\n";
+}
+
+void AMateria::assignPlace(ICharacter *c)
+{
+	m_place = CHAR;
+	m_ptr.c = c;
+}
+
+void AMateria::assignPlace(IMateriaSource * ms)
+{
+	m_place = MATSRC;
+	m_ptr.ms = ms;
+}
+
+void AMateria::assignPlace(Floor *f)
+{
+	m_place = FLOOR;
+	m_ptr.f = f;
 }
--- Module04/ex03/AMateria.hpp
+++ Module04/ex03/AMateria.hpp
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/15 21:06:53 by gwolf             #+#    #+#             */
-/*   Updated: 2023/10/16 20:39:38 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/10/16 23:12:10 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,28 @@
 
 #include <iostream>
 #include <string>
+#include "ICharacter.hpp"
+#include "IMateriaSource.hpp"
+#include "Floor.hpp"
 
 class ICharacter;
 
 class AMateria
 {
+private:
+	enum e_place {CHAR, MATSRC, FLOOR};
+	union u_ptr_place {ICharacter *c; IMateriaSource *ms; Floor *f;};
 protected:
 	const std::string m_type;
+	AMateria::e_place m_place;
+	AMateria::u_ptr_place m_ptr;
 public:
 	AMateria(std::string const & type);
 	virtual ~AMateria();
 	std::string const & getType() const; //Returns the materia type
 	virtual AMateria* clone() const = 0;
 	virtual void use(ICharacter& target);
+	void assignPlace(ICharacter *c);
+	void assignPlace(IMateriaSource * ms);
+	void assignPlace(Floor *f);
 };
