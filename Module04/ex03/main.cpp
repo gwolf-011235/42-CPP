2829068e9b25adfd1cf66d496500c9236dda34ad
Gabriel <gwolf@student.42vienna.com>
2023-10-18 13:12:54 +0200

    feat: copy test from pdf

--- Module04/ex03/main.cpp
+++ Module04/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/15 21:33:53 by gwolf             #+#    #+#             */
-/*   Updated: 2023/10/16 20:12:08 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/10/18 13:07:02 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,29 @@
 #include "Ice.hpp"
 #include "MateriaSource.hpp"
 
-Character a;
 
 int	main(void)
 {
-	std::cout << "hi\n";
+	IMateriaSource* src = new MateriaSource();
+	src->learnMateria(new Ice());
+	src->learnMateria(new Cure());
+
+	ICharacter* me = new Character("me");
+
+	AMateria* tmp;
+	tmp = src->createMateria("ice");
+	me->equip(tmp);
+	tmp = src->createMateria("cure");
+	me->equip(tmp);
+
+	ICharacter* bob = new Character("bob");
+
+	me->use(0, *bob);
+	me->use(1, *bob);
+
+	delete bob;
+	delete me;
+	delete src;
+
 	return 0;
 }
