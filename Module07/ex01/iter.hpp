e6bda67318261c3f4a34c79eb27f02b5c5183bbd
Gabriel Wolf <gwolf@student.42vienna.com>
2024-02-09 18:37:01 +0100

    feat: solution for 07/01

--- Module07/ex01/iter.hpp
+++ Module07/ex01/iter.hpp
@@ -6,16 +6,27 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/07 17:23:43 by gwolf             #+#    #+#             */
-/*   Updated: 2024/02/07 17:24:45 by gwolf            ###   ########.fr       */
+/*   Updated: 2024/02/09 18:31:55 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ITER_HPP
 # define ITER_HPP
 
+# include <cstddef>
+
 template< typename T >
-void	iter(T *array, size_t length, void (*f)(T const &))
+void	iter(T *array, size_t length, void (*f)(const T &))
 {
 	for (size_t i = 0; i < length; i++)
 		f(array[i]);
 }
+
+template< typename T >
+void	iter(T *array, size_t length, void (*f)(T &))
+{
+	for (size_t i = 0; i < length; i++)
+		f(array[i]);
+}
+
+#endif
