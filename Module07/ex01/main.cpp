d57dc3123f1cd6a3504be8725c517ad2fdf7cf74
Gabriel <gwolf@student.42vienna.com>
2024-02-10 13:56:17 +0100

    feat: add custom data struct for testing

--- Module07/ex01/main.cpp
+++ Module07/ex01/main.cpp
@@ -6,11 +6,12 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/09 18:19:51 by gwolf             #+#    #+#             */
-/*   Updated: 2024/02/09 18:36:24 by gwolf            ###   ########.fr       */
+/*   Updated: 2024/02/10 13:55:46 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
+#include "Data.hpp"
 #include <string>
 #include <iostream>
 
@@ -40,12 +41,24 @@ void	incrementString(std::string &x)
 	x += "!";
 }
 
+void	incrementData(Data &x)
+{
+	x.count += 100;
+}
+
 int		main()
 {
-	int		intArray[] = {1, 2, 3, 4, 5};
-	float	floatArray[] = {1.1, 2.2, 3.3, 4.4, 5.5};
-	double	doubleArray[] = {1.1, 2.2, 3.3, 4.4, 5.5};
+	int			intArray[] = {1, 2, 3, 4, 5};
+	float		floatArray[] = {1.1, 2.2, 3.3, 4.4, 5.5};
+	double		doubleArray[] = {1.1, 2.2, 3.3, 4.4, 5.5};
 	std::string	stringArray[] = {"one", "two", "three", "four", "five"};
+	Data		DataArray[] = {
+		{101, "Potato", "Red potato", 100},
+		{121, "Tomato", "Cherry tomato", 200},
+		{123, "Rice", "White rice", 300},
+		{456, "Beans", "Black beans", 400},
+		{789, "Pasta", "Spaghetti", 500}
+	};
 
 	std::cout << "Original arrays:\n";
 	std::cout << "intArray:\n";
@@ -56,6 +69,8 @@ int		main()
 	iter(doubleArray, 5, print);
 	std::cout << "stringArray:\n";
 	iter(stringArray, 5, print);
+	std::cout << "DataArray:\n";
+	iter(DataArray, 5, print);
 	std::cout << "\n";
 
 	std::cout << "Incrementing intArray by 1\n";
@@ -74,6 +89,10 @@ int		main()
 	iter(stringArray, 5, incrementString);
 	std::cout << "new stringArray:\n";
 	iter(stringArray, 5, print);
+	std::cout << "Incrementing DataArray by 100\n";
+	iter(DataArray, 5, incrementData);
+	std::cout << "new DataArray:\n";
+	iter(DataArray, 5, print);
 
 	return 0;
 }
