93cd0759d0c49a2de38b71f087b68a12ece985fb
Gabriel <gwolf@student.42vienna.com>
2024-03-11 13:59:28 +0100

    feat: add draft for 09/02

--- Module09/ex02/PmergeMe.hpp
+++ Module09/ex02/PmergeMe.hpp
@@ -6,13 +6,81 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 11:04:36 by gwolf             #+#    #+#             */
-/*   Updated: 2024/03/04 16:48:59 by gwolf            ###   ########.fr       */
+/*   Updated: 2024/03/11 12:51:23 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PMERGEME_HPP
 # define PMERGEME_HPP
 
+# include <vector>
+# include <iostream>
 
+void	ft_merge(std::vector<int> &vec, int begin, int mid, int end)
+{
+	int i, j, k;
+	int n1 = mid - begin + 1;
+	int n2 = end - mid;
+
+	std::vector<int> L(n1);
+	std::vector<int> R(n2);
+
+	for (i = 0; i < n1; i++) {
+		L[i] = vec[begin + i];
+	}
+	for (j = 0; j < n2; j++) {
+		R[j] = vec[mid + 1 + j];
+	}
+	i = 0;
+	j = 0;
+	k = begin;
+	while (i < n1 && j < n2) {
+		if (L[i] <= R[j]) {
+			vec[k] = L[i];
+			i++;
+		} else {
+			vec[k] = R[j];
+			j++;
+		}
+		k++;
+	}
+	while (i < n1) {
+		vec[k] = L[i];
+		i++;
+		k++;
+	}
+	while (j < n2) {
+		vec[k] = R[j];
+		j++;
+		k++;
+	}
+}
+
+void	ft_mergeSort(std::vector<int> &vec, int begin, int end)
+{
+	if (begin < end) {
+		int mid = begin + (end - begin) / 2;
+		ft_mergeSort(vec, begin, mid);
+		ft_mergeSort(vec, mid + 1, end);
+		ft_merge(vec, begin, mid, end);
+	}
+}
+
+void	ft_FordJohnsonVector(std::vector<int> &vec)
+{
+	bool has_stray = vec.size() % 2;
+
+	std::vector<int>::iterator it = vec.begin();
+	std::vector<int>::iterator ite = vec.end();
+	if (has_stray) {
+		ite--;
+	}
+	for (; it != ite; it += 2) {
+		if (*it > *(it + 1)) {
+			std::iter_swap(it, it + 1);
+		}
+	}
+
+}
 
 #endif
--- Module09/ex02/main.cpp
+++ Module09/ex02/main.cpp
@@ -6,13 +6,32 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 11:03:50 by gwolf             #+#    #+#             */
-/*   Updated: 2024/03/04 11:04:19 by gwolf            ###   ########.fr       */
+/*   Updated: 2024/03/11 12:52:27 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
 
-int	main(void)
+#include <cstdlib>
+
+int	main(int argc, char **argv)
 {
+	std::vector<int>	vec;
+	for (int i = 1; argc > 1; ++i) {
+		vec.push_back(std::atoi(argv[i]));
+		argc--;
+	}
+	std::cout << "Before: ";
+	for (std::vector<int>::iterator it = vec.begin(); it != vec.end(); it++) {
+		std::cout << *it << " ";
+	}
+	std::cout << std::endl;
+	ft_FordJohnsonVector(vec);
+	ft_mergeSort(vec, 0, vec.size() - 1);
+	std::cout << "After: ";
+	for (std::vector<int>::iterator it = vec.begin(); it != vec.end(); it++) {
+		std::cout << *it << " ";
+	}
+	std::cout << std::endl;
 	return (0);
 }
